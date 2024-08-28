33d71ddf1205c6bc9208b5ee01ca41e0042d6742
Eddy <edwardyue93@gmail.com>
2024-08-28 20:16:08 +0200

    fix: :bug: change main function names

--- function_app.py
+++ function_app.py
@@ -9,12 +9,12 @@ app = func.FunctionApp()
 @app.function_name(name="EnscliEmbedProject")
 @app.route(route="embed/{project}", methods=["POST"])
 @basic_auth()
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def embed_project(req: func.HttpRequest) -> func.HttpResponse:
     return post_embed_project(req)
 
 
 @app.function_name(name="EnscliQueryProject")
 @app.route(route="query/{project}", methods=["POST"])
 @basic_auth()
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def query_project(req: func.HttpRequest) -> func.HttpResponse:
     return post_query_project(req)
